e132ee5610017277811ec5fa2c5ea1698bd50abe
Eric Venarusso <eric.venarusso-ext@viavarejo.com.br>
2019-06-06 14:25:35 -0300

    tests

---
+++ test_example.py
@@ -0,0 +1,9 @@
+import pytest
+
+def sum(num1, num2):
+    """It returns sum of two numbers"""
+    return num1 + num2
+
+#make sure to start function name with test
+def test_sum():
+    assert sum(1, 2) == 3
