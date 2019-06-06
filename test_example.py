bb11a0f5407fe9de325f9629ae34ce5cffbf14d5
joaopcnogueira <joaonogueira@fisica.ufc.br>
2019-06-06 17:46:45 +0000

    Quebrando tudo!

--- test_example.py
+++ test_example.py
@@ -2,7 +2,7 @@ import pytest
 
 def sum(num1, num2):
     """It returns sum of two numbers"""
-    return num1 + num2
+    return num1 - num2
 
 #make sure to start function name with test
 def test_sum():
